ed88f644cae1e3c00f140f60aea3444892945e1d
Jason Blanchard <jas.blanchard@gmail.com>
2016-05-22 17:42:38 -0400

    Added hamilipsum

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -5,11 +5,11 @@ import './ProjectsPage.scss';
 
 const projects = [
   {
-    title: 'Writermortis',
-    description: 'Writermortis is an online writing game that works like an "exquisite corpse" - one writer begins the story and sends it to other writers. Each subsequent writer can only see the previously written section. Writermortis is currently a Ruby on Rails app but is going through an extensive re-write in Ember.js.',
-    web: 'http://www.writermortis.com/',
-    source: 'https://github.com/jasonblanchard/writermortis',
-    image: 'public/writermortis.png',
+    title: 'Hamilipsum',
+    description: 'Sourced from the writings of Alexander Hamilton, "Hamilipsum" is a lorem ipsum generator for designers and developers.',
+    web: 'http://hamilipsum.com/',
+    source: 'https://github.com/jasonblanchard/hamilipsum',
+    image: 'public/hamilipsum.png',
   },
   {
     title: 'Imperfect Tense',
@@ -17,6 +17,13 @@ const projects = [
     web: 'http://jasonblanchard.github.io/imperfect-tense/',
     source: 'https://github.com/jasonblanchard/imperfect-tense',
     image: 'public/imperfect_tense.png',
+  },
+  {
+    title: 'Writermortis',
+    description: 'Writermortis is an online writing game that works like an "exquisite corpse" - one writer begins the story and sends it to other writers. Each subsequent writer can only see the previously written section. Writermortis is currently a Ruby on Rails app but is going through an extensive re-write in Ember.js.',
+    web: 'http://www.writermortis.com/',
+    source: 'https://github.com/jasonblanchard/writermortis',
+    image: 'public/writermortis.png',
   },
 ];
 
@@ -41,7 +48,7 @@ export default class ProjectsPage extends Component {
           <h2>{project.title}</h2>
           <p>{project.description}</p>
           <div className="ProjectsPage-projectLinks">
-            <a href={project.web}>web</a> | <a href={project.source}>source</a>
+            <a className="ProjectsPage-projectLinkWeb" href={project.web} target="blank">web</a> | <a className="ProjectsPage-projectLinkSource" href={project.source} target="blank">source</a>
           </div>
         </div>
       </div>
