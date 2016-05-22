ee3395f35c5028fba02c88b6bc84e293fa63c90e
Jason Blanchard <jas.blanchard@gmail.com>
2016-05-22 18:22:15 -0400

    Style for about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
+import './AboutBlank.scss';
+
 export default class AboutPage extends Component {
   render() {
     return (
-      <section>
-        <p>about</p>
-        <p> Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
-        <img alt="profile" src="public/profile.png" />
+      <section className="AboutPage">
+        <div className="AboutPage-bio">
+          <img alt="profile" src="public/profile.png" />
+          <p>I am a Web developer and instructional technologist crafting tools and ideas that empower learners in a networked world.</p>
+          <p>Technologies that are exciting to me right now include <strong>Node.js</strong> and <strong>React</strong>.</p>
+          <p>I'm also a contributer to GitLab and other open source projects.</p>
+        </div>
+        <div className="AboutPage-timeline">
+          <iframe src="http://cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=0AlNyK0CWPkZjdE82U1dQNkRKRHdDaldmZUlnZUlBX0E&font=Bevan-PotanoSans&maptype=toner&lang=en&start_at_slide=8&height=650" width="100%" height="650" frameBorder="s0"></iframe>
+        </div>
       </section>
     );
   }
