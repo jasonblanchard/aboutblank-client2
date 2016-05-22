e726341231215e2a13389f0046f8ce5ce30e3349
Jason Blanchard <jas.blanchard@gmail.com>
2016-05-22 17:59:48 -0400

    Fixed transition

--- src/layouts/SideRevealLayout.jsx
+++ src/layouts/SideRevealLayout.jsx
@@ -11,9 +11,9 @@ export default class SideRevealLayout extends Component {
       <ReactCSSTransitionGroup
         component="div"
         transitionName="SideRevealLayout"
-        transitionEnterTimeout={500}
-        transitionLeaveTimeout={500}
-        transitionAppearTimeout={500}
+        transitionEnterTimeout={300}
+        transitionLeaveTimeout={300}
+        transitionAppearTimeout={300}
         transitionAppear
         role="main"
         className={className}
