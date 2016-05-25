ee2bf758ade396fb986555629a597d2c1eadb8b8
Jason Blanchard <jas.blanchard@gmail.com>
2016-05-24 21:25:01 -0400

    Updated deploy script added google analytics

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-aws s3 sync build s3://about.blanktech.net2/ --profile aboutblank  --acl public-read --delete
+aws s3 sync build s3://about.blanktech.net --profile aboutblank  --acl public-read --delete
